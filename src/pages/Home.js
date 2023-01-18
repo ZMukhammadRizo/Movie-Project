ebf051bd02555ad769c0150ec95e1d54d6941755
unknown <zokirjonovmuhammadrizo6@gmail.com>
2023-01-18 15:19:32 +0500

    bugs fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Search from '../components/Search';
 import Information from '../components/Information';
-import WrapBanner from "../components/That's a Wrap/Banner";
 
 const Home = () => {
   return (
     <>
       <Search />
       <Information />
-      <WrapBanner />
     </>
   );
 };
